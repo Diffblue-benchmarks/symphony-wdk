4664f275875a5af01940dd7ccae160ef98b4a263
Diffblue CI <ci@diffblue.com>
2026-03-13 12:24:15 +0000

    test(workflow-bot-app): add generated tests for com.symphony.bdk.workflow.engine.camunda.bpmn.builder.ActivityNodeBuilder.addScriptTask(Lorg/camunda/bpm/model/bpmn/builder/AbstractFlowNodeBuilder;Lcom/symphony/bdk/workflow/swadl/v1/activity/ExecuteScript;)Lorg/camunda/bpm/model/bpmn/builder/AbstractFlowNodeBuilder;

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/ActivityNodeBuilderTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/ActivityNodeBuilderTest.java
@@ -1,9 +1,13 @@
 package com.symphony.bdk.workflow.engine.camunda.bpmn.builder;
 
 import com.symphony.bdk.workflow.engine.camunda.CamundaExecutor;
+import com.symphony.bdk.workflow.engine.camunda.audit.ScriptTaskAuditListener;
 import com.symphony.bdk.workflow.swadl.ActivityRegistry;
+import com.symphony.bdk.workflow.swadl.v1.activity.ExecuteScript;
 import com.symphony.bdk.workflow.swadl.v1.activity.message.SendMessage;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.model.bpmn.builder.AbstractFlowNodeBuilder;
+import org.camunda.bpm.model.bpmn.builder.ScriptTaskBuilder;
 import org.camunda.bpm.model.bpmn.builder.ServiceTaskBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -77,5 +81,65 @@ class ActivityNodeBuilderTest {
     verify(mockServiceTaskBuilder).id("customActivityId");
     verify(mockServiceTaskBuilder).name("customActivityId");
     verify(mockServiceTaskBuilder).camundaInputParameter(eq(CamundaExecutor.ACTIVITY), eq("customActivityId"));
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void addScriptTaskShouldConfigureScriptTaskWithScriptProperties() {
+    ExecuteScript scriptActivity = new ExecuteScript();
+    scriptActivity.setId("testScriptTask");
+    scriptActivity.setScript("println 'Hello World'");
+
+    AbstractFlowNodeBuilder<?, ?> mockBuilder = mock(AbstractFlowNodeBuilder.class);
+    ScriptTaskBuilder mockScriptTaskBuilder = mock(ScriptTaskBuilder.class);
+
+    when(mockBuilder.scriptTask()).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.id(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.name(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.camundaAsyncAfter()).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.scriptText(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.scriptFormat(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.camundaExecutionListenerClass(anyString(), eq(ScriptTaskAuditListener.class)))
+        .thenReturn(mockScriptTaskBuilder);
+
+    AbstractFlowNodeBuilder<?, ?> result = activityNodeBuilder.addTask(mockBuilder, scriptActivity);
+
+    assertNotNull(result);
+    verify(mockBuilder).scriptTask();
+    verify(mockScriptTaskBuilder).id("testScriptTask");
+    verify(mockScriptTaskBuilder).name("testScriptTask");
+    verify(mockScriptTaskBuilder).camundaAsyncAfter();
+    verify(mockScriptTaskBuilder).scriptText("println 'Hello World'");
+    verify(mockScriptTaskBuilder).scriptFormat(ExecuteScript.SCRIPT_ENGINE);
+    verify(mockScriptTaskBuilder).camundaExecutionListenerClass(
+        eq(ExecutionListener.EVENTNAME_START),
+        eq(ScriptTaskAuditListener.class));
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void addScriptTaskShouldHandleDifferentScriptContent() {
+    ExecuteScript scriptActivity = new ExecuteScript();
+    scriptActivity.setId("customScriptTask");
+    scriptActivity.setScript("def x = 42; return x * 2");
+
+    AbstractFlowNodeBuilder<?, ?> mockBuilder = mock(AbstractFlowNodeBuilder.class);
+    ScriptTaskBuilder mockScriptTaskBuilder = mock(ScriptTaskBuilder.class);
+
+    when(mockBuilder.scriptTask()).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.id(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.name(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.camundaAsyncAfter()).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.scriptText(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.scriptFormat(anyString())).thenReturn(mockScriptTaskBuilder);
+    when(mockScriptTaskBuilder.camundaExecutionListenerClass(anyString(), eq(ScriptTaskAuditListener.class)))
+        .thenReturn(mockScriptTaskBuilder);
+
+    AbstractFlowNodeBuilder<?, ?> result = activityNodeBuilder.addTask(mockBuilder, scriptActivity);
+
+    assertNotNull(result);
+    verify(mockScriptTaskBuilder).id("customScriptTask");
+    verify(mockScriptTaskBuilder).name("customScriptTask");
+    verify(mockScriptTaskBuilder).scriptText("def x = 42; return x * 2");
   }
 }
