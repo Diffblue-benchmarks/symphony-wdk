90c1976c5eb6ec8a6e62e34f0a8006c916da4cb8
Diffblue CI <ci@diffblue.com>
2026-03-13 12:30:08 +0000

    test(workflow-bot-app): add generated tests for com.symphony.bdk.workflow.event.V4ConnectionRequestedProcessor.<init>(Lorg/camunda/bpm/engine/RuntimeService;)V

---
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/event/V4ConnectionRequestedProcessorTest.java
@@ -0,0 +1,27 @@
+package com.symphony.bdk.workflow.event;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class V4ConnectionRequestedProcessorTest {
+
+  @Mock
+  private RuntimeService runtimeService;
+
+  @Test
+  void shouldCreateProcessorWithRuntimeService() {
+    // Arrange & Act
+    V4ConnectionRequestedProcessor processor = new V4ConnectionRequestedProcessor(runtimeService);
+
+    // Assert
+    assertThat(processor).isNotNull();
+    assertThat(processor.runtimeService).isEqualTo(runtimeService);
+    assertThat(processor.eventName).isEqualTo("connection-requested");
+  }
+}
