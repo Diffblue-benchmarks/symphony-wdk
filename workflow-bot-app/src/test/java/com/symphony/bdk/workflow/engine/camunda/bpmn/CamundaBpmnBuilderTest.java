4d40da50a98a6b143dbddc13a677bc3986eb0881
Diffblue CI <ci@diffblue.com>
2026-03-13 13:42:40 +0000

    test(workflow-bot-app): add generated tests for com.symphony.bdk.workflow.engine.camunda.bpmn.CamundaBpmnBuilder.closeUpSubProcessesIfAny(Lcom/symphony/bdk/workflow/engine/camunda/bpmn/BuildProcessContext;Lorg/camunda/bpm/model/bpmn/builder/AbstractFlowNodeBuilder;)Lorg/camunda/bpm/model/bpmn/builder/AbstractFlowNodeBuilder;

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/CamundaBpmnBuilderTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/CamundaBpmnBuilderTest.java
@@ -8,6 +8,7 @@ import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.model.bpmn.builder.AbstractFlowNodeBuilder;
 import org.camunda.bpm.model.bpmn.builder.EndEventBuilder;
 import org.camunda.bpm.model.bpmn.builder.EventBasedGatewayBuilder;
+import org.camunda.bpm.model.bpmn.builder.EventSubProcessBuilder;
 import org.camunda.bpm.model.bpmn.builder.ExclusiveGatewayBuilder;
 import org.camunda.bpm.model.bpmn.builder.SubProcessBuilder;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ import java.lang.reflect.Method;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -167,5 +169,130 @@ class CamundaBpmnBuilderTest {
     addGatewayMethod.setAccessible(true);
     return (AbstractFlowNodeBuilder<?, ?>) addGatewayMethod.invoke(camundaBpmnBuilder, nodeId, builder, activities,
         conditional, childrenSize);
+  }
+
+  @Test
+  void shouldReturnBuilderUnchangedWhenNoSubProcesses() throws Exception {
+    // Given
+    when(context.hasEventSubProcess()).thenReturn(false);
+    when(context.hasTimeoutSubProcess()).thenReturn(false);
+
+    // When
+    AbstractFlowNodeBuilder<?, ?> result = invokeCloseUpSubProcessesIfAny(context, flowNodeBuilder);
+
+    // Then
+    assertThat(result).isEqualTo(flowNodeBuilder);
+    verify(context, never()).removeLastEventSubProcessBuilder();
+    verify(context, never()).removeLastSubProcessTimeoutBuilder();
+  }
+
+  @Test
+  void shouldCloseEventSubProcessesWhenPresent() throws Exception {
+    // Given
+    EventSubProcessBuilder eventSubProcessBuilder = org.mockito.Mockito.mock(EventSubProcessBuilder.class);
+    SubProcessBuilder resultSubProcessBuilder = org.mockito.Mockito.mock(SubProcessBuilder.class);
+    EndEventBuilder resultEndEventBuilder = org.mockito.Mockito.mock(EndEventBuilder.class);
+
+    when(context.hasEventSubProcess()).thenReturn(true, false);
+    when(context.hasTimeoutSubProcess()).thenReturn(false);
+    when(context.removeLastEventSubProcessBuilder()).thenReturn(eventSubProcessBuilder);
+    when(eventSubProcessBuilder.subProcessDone()).thenReturn(resultSubProcessBuilder);
+    when(resultSubProcessBuilder.endEvent()).thenReturn(resultEndEventBuilder);
+
+    // When
+    AbstractFlowNodeBuilder<?, ?> result = invokeCloseUpSubProcessesIfAny(context, flowNodeBuilder);
+
+    // Then
+    verify(context).removeLastEventSubProcessBuilder();
+    verify(eventSubProcessBuilder).subProcessDone();
+    verify(context).cacheSubProcessTimeoutToDone(resultSubProcessBuilder);
+    verify(resultSubProcessBuilder).endEvent();
+    assertThat(result).isEqualTo(resultEndEventBuilder);
+  }
+
+  @Test
+  void shouldCloseTimeoutSubProcessesWhenPresent() throws Exception {
+    // Given
+    AbstractFlowNodeBuilder<?, ?> timeoutSubProcessBuilder = org.mockito.Mockito.mock(AbstractFlowNodeBuilder.class);
+
+    when(context.hasEventSubProcess()).thenReturn(false);
+    when(context.hasTimeoutSubProcess()).thenReturn(true, false);
+    doReturn(timeoutSubProcessBuilder).when(context).removeLastSubProcessTimeoutBuilder();
+    when(timeoutSubProcessBuilder.endEvent()).thenReturn(endEventBuilder);
+
+    // When
+    AbstractFlowNodeBuilder<?, ?> result = invokeCloseUpSubProcessesIfAny(context, flowNodeBuilder);
+
+    // Then
+    verify(context).removeLastSubProcessTimeoutBuilder();
+    verify(timeoutSubProcessBuilder).endEvent();
+    assertThat(result).isEqualTo(flowNodeBuilder);
+  }
+
+  @Test
+  void shouldCloseBothEventAndTimeoutSubProcessesWhenPresent() throws Exception {
+    // Given
+    EventSubProcessBuilder eventSubProcessBuilder = org.mockito.Mockito.mock(EventSubProcessBuilder.class);
+    SubProcessBuilder resultSubProcessBuilder = org.mockito.Mockito.mock(SubProcessBuilder.class);
+    EndEventBuilder eventEndEventBuilder = org.mockito.Mockito.mock(EndEventBuilder.class);
+    AbstractFlowNodeBuilder<?, ?> timeoutSubProcessBuilder = org.mockito.Mockito.mock(AbstractFlowNodeBuilder.class);
+
+    when(context.hasEventSubProcess()).thenReturn(true, false);
+    when(context.hasTimeoutSubProcess()).thenReturn(true, false);
+    when(context.removeLastEventSubProcessBuilder()).thenReturn(eventSubProcessBuilder);
+    when(eventSubProcessBuilder.subProcessDone()).thenReturn(resultSubProcessBuilder);
+    when(resultSubProcessBuilder.endEvent()).thenReturn(eventEndEventBuilder);
+    doReturn(timeoutSubProcessBuilder).when(context).removeLastSubProcessTimeoutBuilder();
+    when(timeoutSubProcessBuilder.endEvent()).thenReturn(endEventBuilder);
+
+    // When
+    AbstractFlowNodeBuilder<?, ?> result = invokeCloseUpSubProcessesIfAny(context, flowNodeBuilder);
+
+    // Then
+    verify(context).removeLastEventSubProcessBuilder();
+    verify(eventSubProcessBuilder).subProcessDone();
+    verify(context).cacheSubProcessTimeoutToDone(resultSubProcessBuilder);
+    verify(resultSubProcessBuilder).endEvent();
+    verify(context).removeLastSubProcessTimeoutBuilder();
+    verify(timeoutSubProcessBuilder).endEvent();
+    assertThat(result).isEqualTo(eventEndEventBuilder);
+  }
+
+  @Test
+  void shouldCloseMultipleEventSubProcessesWhenPresent() throws Exception {
+    // Given
+    EventSubProcessBuilder eventSubProcessBuilder1 = org.mockito.Mockito.mock(EventSubProcessBuilder.class);
+    EventSubProcessBuilder eventSubProcessBuilder2 = org.mockito.Mockito.mock(EventSubProcessBuilder.class);
+    SubProcessBuilder resultSubProcessBuilder1 = org.mockito.Mockito.mock(SubProcessBuilder.class);
+    SubProcessBuilder resultSubProcessBuilder2 = org.mockito.Mockito.mock(SubProcessBuilder.class);
+    EndEventBuilder eventEndEventBuilder1 = org.mockito.Mockito.mock(EndEventBuilder.class);
+    EndEventBuilder eventEndEventBuilder2 = org.mockito.Mockito.mock(EndEventBuilder.class);
+
+    when(context.hasEventSubProcess()).thenReturn(true, true, false);
+    when(context.hasTimeoutSubProcess()).thenReturn(false);
+    when(context.removeLastEventSubProcessBuilder()).thenReturn(eventSubProcessBuilder1, eventSubProcessBuilder2);
+    when(eventSubProcessBuilder1.subProcessDone()).thenReturn(resultSubProcessBuilder1);
+    when(eventSubProcessBuilder2.subProcessDone()).thenReturn(resultSubProcessBuilder2);
+    when(resultSubProcessBuilder1.endEvent()).thenReturn(eventEndEventBuilder1);
+    when(resultSubProcessBuilder2.endEvent()).thenReturn(eventEndEventBuilder2);
+
+    // When
+    AbstractFlowNodeBuilder<?, ?> result = invokeCloseUpSubProcessesIfAny(context, flowNodeBuilder);
+
+    // Then
+    verify(context, org.mockito.Mockito.times(2)).removeLastEventSubProcessBuilder();
+    verify(eventSubProcessBuilder1).subProcessDone();
+    verify(eventSubProcessBuilder2).subProcessDone();
+    verify(context).cacheSubProcessTimeoutToDone(resultSubProcessBuilder1);
+    verify(context).cacheSubProcessTimeoutToDone(resultSubProcessBuilder2);
+    assertThat(result).isEqualTo(eventEndEventBuilder2);
+  }
+
+  private AbstractFlowNodeBuilder<?, ?> invokeCloseUpSubProcessesIfAny(BuildProcessContext context,
+      AbstractFlowNodeBuilder<?, ?> builder) throws Exception {
+    Method closeUpSubProcessesIfAnyMethod = CamundaBpmnBuilder.class.getDeclaredMethod("closeUpSubProcessesIfAny",
+        BuildProcessContext.class, AbstractFlowNodeBuilder.class);
+    closeUpSubProcessesIfAnyMethod.setAccessible(true);
+    return (AbstractFlowNodeBuilder<?, ?>) closeUpSubProcessesIfAnyMethod.invoke(camundaBpmnBuilder, context, builder);
   }
 }
