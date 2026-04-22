5172dd0e482f6ec01d1c2b4d5e8bf31f1eaa02e6
Diffblue CI <ci@diffblue.com>
2026-04-22 14:54:19 +0000

    test(workflow-bot-app): Add generated tests for com.symphony.bdk.workflow.engine.executor.room.RemoveRoomMemberExecutor.doOboWithCache(Lcom/symphony/bdk/workflow/engine/executor/ActivityExecutorContext;)Ljava/lang/Void;

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/executor/room/RemoveRoomMemberExecutorDiffblueTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/executor/room/RemoveRoomMemberExecutorDiffblueTest.java
@@ -7,7 +7,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
+import com.symphony.bdk.core.OboServices;
+import com.symphony.bdk.core.auth.AuthSession;
+import com.symphony.bdk.core.service.stream.StreamService;
 import com.symphony.bdk.workflow.engine.executor.ActivityExecutorContext;
+import com.symphony.bdk.workflow.engine.executor.BdkGateway;
 import com.symphony.bdk.workflow.swadl.v1.activity.Obo;
 import com.symphony.bdk.workflow.swadl.v1.activity.room.RemoveRoomMember;
 import java.util.List;
@@ -15,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -139,5 +144,97 @@ class RemoveRoomMemberExecutorDiffblueTest {
         IllegalArgumentException.class,
         () -> removeRoomMemberExecutor.doOboWithCache(execution));
     verify(execution).getActivity();
+  }
+
+  /**
+   * Test {@link RemoveRoomMemberExecutor#doOboWithCache(ActivityExecutorContext)}.
+   *
+   * <ul>
+   *   <li>Given {@link RemoveRoomMember} with OBO username and userIds set.
+   *   <li>Then calls {@code obo(authSession).streams().removeMemberFromRoom(...)} and returns null.
+   * </ul>
+   *
+   * <p>Method under test: {@link RemoveRoomMemberExecutor#doOboWithCache(ActivityExecutorContext)}
+   */
+  @Test
+  @DisplayName(
+      "Test doOboWithCache(ActivityExecutorContext); given OBO username and userIds; then call removeMemberFromRoom")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"Void RemoveRoomMemberExecutor.doOboWithCache(ActivityExecutorContext)"})
+  void testDoOboWithCache_givenOboUsernameAndUserIds_thenCallRemoveMemberFromRoom() {
+    // Arrange
+    Obo obo = new Obo();
+    obo.setUsername("janedoe");
+
+    RemoveRoomMember removeRoomMember = new RemoveRoomMember();
+    removeRoomMember.setStreamId("42");
+    removeRoomMember.setUserIds(List.of(1L));
+    removeRoomMember.setObo(obo);
+
+    StreamService streamService = mock(StreamService.class);
+    OboServices oboServices = mock(OboServices.class);
+    when(oboServices.streams()).thenReturn(streamService);
+
+    AuthSession authSession = mock(AuthSession.class);
+    BdkGateway bdkGateway = mock(BdkGateway.class);
+    when(bdkGateway.obo(Mockito.<String>any())).thenReturn(authSession);
+    when(bdkGateway.obo(Mockito.<AuthSession>any())).thenReturn(oboServices);
+
+    ActivityExecutorContext<RemoveRoomMember> execution = mock(ActivityExecutorContext.class);
+    when(execution.getActivity()).thenReturn(removeRoomMember);
+    when(execution.bdk()).thenReturn(bdkGateway);
+
+    // Act
+    removeRoomMemberExecutor.doOboWithCache(execution);
+
+    // Assert
+    verify(streamService).removeMemberFromRoom(1L, "42");
+  }
+
+  /**
+   * Test {@link RemoveRoomMemberExecutor#doOboWithCache(ActivityExecutorContext)}.
+   *
+   * <ul>
+   *   <li>Given {@link RemoveRoomMember} with OBO userId and userIds set.
+   *   <li>Then calls {@code obo(authSession).streams().removeMemberFromRoom(...)} and returns null.
+   * </ul>
+   *
+   * <p>Method under test: {@link RemoveRoomMemberExecutor#doOboWithCache(ActivityExecutorContext)}
+   */
+  @Test
+  @DisplayName(
+      "Test doOboWithCache(ActivityExecutorContext); given OBO userId and userIds; then call removeMemberFromRoom")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"Void RemoveRoomMemberExecutor.doOboWithCache(ActivityExecutorContext)"})
+  void testDoOboWithCache_givenOboUserIdAndUserIds_thenCallRemoveMemberFromRoom() {
+    // Arrange
+    Obo obo = new Obo();
+    obo.setUserId(1L);
+
+    RemoveRoomMember removeRoomMember = new RemoveRoomMember();
+    removeRoomMember.setStreamId("42");
+    removeRoomMember.setUserIds(List.of(2L));
+    removeRoomMember.setObo(obo);
+
+    StreamService streamService = mock(StreamService.class);
+    OboServices oboServices = mock(OboServices.class);
+    when(oboServices.streams()).thenReturn(streamService);
+
+    AuthSession authSession = mock(AuthSession.class);
+    BdkGateway bdkGateway = mock(BdkGateway.class);
+    when(bdkGateway.obo(Mockito.<Long>any())).thenReturn(authSession);
+    when(bdkGateway.obo(Mockito.<AuthSession>any())).thenReturn(oboServices);
+
+    ActivityExecutorContext<RemoveRoomMember> execution = mock(ActivityExecutorContext.class);
+    when(execution.getActivity()).thenReturn(removeRoomMember);
+    when(execution.bdk()).thenReturn(bdkGateway);
+
+    // Act
+    removeRoomMemberExecutor.doOboWithCache(execution);
+
+    // Assert
+    verify(streamService).removeMemberFromRoom(2L, "42");
   }
 }
