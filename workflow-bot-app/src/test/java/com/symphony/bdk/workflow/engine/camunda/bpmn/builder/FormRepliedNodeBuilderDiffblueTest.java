4437b34c918728c8ff8639c6fa5976312f8232c5
Diffblue CI <ci@diffblue.com>
2026-04-22 15:22:44 +0000

    test(workflow-bot-app): Add generated tests for com.symphony.bdk.workflow.engine.camunda.bpmn.builder.FormRepliedNodeBuilder$ThrowTimeoutDelegate.notify(Lorg/camunda/bpm/engine/delegate/DelegateExecution;)V

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/FormRepliedNodeBuilderDiffblueTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/FormRepliedNodeBuilderDiffblueTest.java
@@ -1,9 +1,11 @@
 package com.symphony.bdk.workflow.engine.camunda.bpmn.builder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import com.symphony.bdk.workflow.engine.WorkflowNodeType;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -30,5 +32,25 @@ class FormRepliedNodeBuilderDiffblueTest {
   void testType() {
     // Arrange, Act and Assert
     assertEquals(WorkflowNodeType.FORM_REPLIED_EVENT, formRepliedNodeBuilder.type());
+  }
+
+  /**
+   * Test {@link FormRepliedNodeBuilder.ThrowTimeoutDelegate#notify(org.camunda.bpm.engine.delegate.DelegateExecution)}.
+   *
+   * <p>Method under test: {@link FormRepliedNodeBuilder.ThrowTimeoutDelegate#notify(org.camunda.bpm.engine.delegate.DelegateExecution)}
+   */
+  @Test
+  @DisplayName("Test ThrowTimeoutDelegate notify(DelegateExecution)")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"void FormRepliedNodeBuilder.ThrowTimeoutDelegate.notify(DelegateExecution)"})
+  void testThrowTimeoutDelegateNotify() {
+    // Arrange
+    FormRepliedNodeBuilder.ThrowTimeoutDelegate delegate = new FormRepliedNodeBuilder.ThrowTimeoutDelegate();
+
+    // Act and Assert
+    BpmnError thrownError = assertThrows(BpmnError.class, () -> delegate.notify(null));
+    assertEquals("408", thrownError.getErrorCode());
+    assertEquals("Form reply event is timeout.", thrownError.getMessage());
   }
 }
