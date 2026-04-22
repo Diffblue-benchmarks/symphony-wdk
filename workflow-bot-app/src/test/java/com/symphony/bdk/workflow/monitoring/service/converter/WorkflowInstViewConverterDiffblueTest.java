bc169ae1ee425fdaad6ecba29123ba0c0e8b5ff3
Diffblue CI <ci@diffblue.com>
2026-04-22 16:44:34 +0000

    test(workflow-bot-app): Add generated tests for com.symphony.bdk.workflow.monitoring.service.converter.WorkflowInstViewConverter.apply(Lcom/symphony/bdk/workflow/monitoring/repository/domain/WorkflowInstanceDomain;)Lcom/symphony/bdk/workflow/api/v1/dto/WorkflowInstView;

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/monitoring/service/converter/WorkflowInstViewConverterDiffblueTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/monitoring/service/converter/WorkflowInstViewConverterDiffblueTest.java
@@ -79,6 +79,55 @@ class WorkflowInstViewConverterDiffblueTest {
    * WorkflowInstanceDomain}.
    *
    * <ul>
+   *   <li>Then return Status is {@code FAILED}.
+   * </ul>
+   *
+   * <p>Method under test: {@link WorkflowInstViewConverter#apply(WorkflowInstanceDomain)}
+   */
+  @Test
+  @DisplayName(
+      "Test apply(WorkflowInstanceDomain) with 'WorkflowInstanceDomain'; then return Status is 'FAILED'")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"WorkflowInstView WorkflowInstViewConverter.apply(WorkflowInstanceDomain)"})
+  void testApplyWithWorkflowInstanceDomain_thenReturnStatusIsFailed() {
+    // Arrange
+    Duration duration = Duration.ofSeconds(1L);
+
+    // Act
+    WorkflowInstView actualApplyResult =
+        workflowInstViewConverter.apply(
+            WorkflowInstanceDomain.builder()
+                .duration(duration)
+                .endDate(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant())
+                .id("42")
+                .instanceId("42")
+                .name("Name")
+                .startDate(
+                    LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant())
+                .status("FAILED")
+                .version(1L)
+                .build());
+
+    // Assert
+    assertEquals("42", actualApplyResult.getInstanceId());
+    assertEquals("Name", actualApplyResult.getId());
+    Instant endDate = actualApplyResult.getEndDate();
+    assertEquals(0, endDate.getNano());
+    assertEquals(0L, endDate.getEpochSecond());
+    Duration duration2 = actualApplyResult.getDuration();
+    assertEquals(1000000000L, duration2.toNanos());
+    assertEquals(1L, actualApplyResult.getVersion().longValue());
+    assertEquals(StatusEnum.FAILED, actualApplyResult.getStatus());
+    assertSame(endDate, actualApplyResult.getStartDate());
+    assertSame(duration, duration2);
+  }
+
+  /**
+   * Test {@link WorkflowInstViewConverter#apply(WorkflowInstanceDomain)} with {@code
+   * WorkflowInstanceDomain}.
+   *
+   * <ul>
    *   <li>Then return Status is {@code PENDING}.
    * </ul>
    *
