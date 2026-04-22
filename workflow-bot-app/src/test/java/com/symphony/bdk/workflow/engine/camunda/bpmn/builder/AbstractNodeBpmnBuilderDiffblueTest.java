c430582c0a700c41c064b83829002e4a754d1046
Diffblue CI <ci@diffblue.com>
2026-04-22 15:33:34 +0000

    test(workflow-bot-app): Add generated tests for com.symphony.bdk.workflow.engine.camunda.bpmn.builder.AbstractNodeBpmnBuilder
    - connect
    - doConditionalConnection
    - connectToExistingNode
    - build

--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/AbstractNodeBpmnBuilderDiffblueTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/AbstractNodeBpmnBuilderDiffblueTest.java
@@ -1,17 +1,25 @@
 package com.symphony.bdk.workflow.engine.camunda.bpmn.builder;
 
+import static com.symphony.bdk.workflow.engine.camunda.bpmn.CamundaBpmnBuilder.EXCLUSIVE_GATEWAY_SUFFIX;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import com.symphony.bdk.workflow.engine.WorkflowNode;
+import com.symphony.bdk.workflow.engine.WorkflowNodeType;
 import com.symphony.bdk.workflow.engine.camunda.bpmn.BuildProcessContext;
 import javax.imageio.metadata.IIOMetadataNode;
 import org.camunda.bpm.model.bpmn.builder.AbstractFlowNodeBuilder;
 import org.camunda.bpm.model.bpmn.builder.BoundaryEventBuilder;
 import org.camunda.bpm.model.bpmn.builder.ComplexGatewayBuilder;
+import org.camunda.bpm.model.bpmn.builder.EventSubProcessBuilder;
+import org.camunda.bpm.model.bpmn.builder.ExclusiveGatewayBuilder;
+import org.camunda.bpm.model.bpmn.builder.SubProcessBuilder;
 import org.camunda.bpm.model.bpmn.impl.BpmnModelInstanceImpl;
 import org.camunda.bpm.model.bpmn.impl.instance.BoundaryEventImpl;
 import org.camunda.bpm.model.xml.impl.ModelBuilderImpl;
@@ -209,5 +217,217 @@ class AbstractNodeBpmnBuilderDiffblueTest {
 
     // Assert
     verify(builder).connectTo("42");
+  }
+
+  /**
+   * Test {@link AbstractNodeBpmnBuilder#connectToExistingNode(String, AbstractFlowNodeBuilder)}.
+   *
+   * <ul>
+   *   <li>Given builder is a SubProcessBuilder; then connectTo is NOT called.
+   * </ul>
+   *
+   * <p>Method under test: {@link AbstractNodeBpmnBuilder#connectToExistingNode(String,
+   * AbstractFlowNodeBuilder)}
+   */
+  @Test
+  @DisplayName(
+      "Test connectToExistingNode(String, AbstractFlowNodeBuilder); given SubProcessBuilder then connectTo is skipped")
+  void testConnectToExistingNode_withSubProcessBuilder() {
+    // Arrange
+    SubProcessBuilder builder = mock(SubProcessBuilder.class);
+
+    // Act
+    abstractNodeBpmnBuilder.connectToExistingNode("nodeId", builder);
+
+    // Assert
+    verify(builder, never()).connectTo(anyString());
+  }
+
+  /**
+   * Test {@link AbstractNodeBpmnBuilder#connect(WorkflowNode, String, AbstractFlowNodeBuilder,
+   * BuildProcessContext)}.
+   *
+   * <ul>
+   *   <li>When node is not already built; then build() is called.
+   * </ul>
+   */
+  @Test
+  @DisplayName(
+      "Test connect(WorkflowNode, String, AbstractFlowNodeBuilder, BuildProcessContext); when node not already built")
+  void testConnect_notAlreadyBuilt() {
+    // Arrange
+    AbstractNodeBpmnBuilder underTest = new AbstractNodeBpmnBuilder() {
+      @Override
+      public AbstractFlowNodeBuilder<?, ?> build(WorkflowNode element, String parentId,
+          AbstractFlowNodeBuilder<?, ?> builder, BuildProcessContext context) {
+        return builder;
+      }
+
+      @Override
+      public WorkflowNodeType type() {
+        return WorkflowNodeType.ACTIVITY;
+      }
+    };
+
+    WorkflowNode element = mock(WorkflowNode.class);
+    when(element.getId()).thenReturn("nodeId");
+    when(element.isConditional()).thenReturn(false);
+
+    BoundaryEventBuilder builder = mock(BoundaryEventBuilder.class);
+
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    when(context.isAlreadyBuilt("nodeId")).thenReturn(false);
+    when(context.hasEventSubProcess()).thenReturn(false);
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result = underTest.connect(element, "parentId", builder, context);
+
+    // Assert
+    verify(context).isAlreadyBuilt("nodeId");
+    assertSame(builder, result);
+  }
+
+  /**
+   * Test {@link AbstractNodeBpmnBuilder#connect(WorkflowNode, String, AbstractFlowNodeBuilder,
+   * BuildProcessContext)}.
+   *
+   * <ul>
+   *   <li>When not already built, builder is a gateway and element is conditional; then condition
+   *       is set on the gateway.
+   * </ul>
+   */
+  @Test
+  @DisplayName(
+      "Test connect(WorkflowNode, String, AbstractFlowNodeBuilder, BuildProcessContext); when not already built with gateway and conditional")
+  void testConnect_notAlreadyBuilt_gatewayConditional() {
+    // Arrange
+    AbstractNodeBpmnBuilder underTest = new AbstractNodeBpmnBuilder() {
+      @Override
+      public AbstractFlowNodeBuilder<?, ?> build(WorkflowNode element, String parentId,
+          AbstractFlowNodeBuilder<?, ?> builder, BuildProcessContext context) {
+        return builder;
+      }
+
+      @Override
+      public WorkflowNodeType type() {
+        return WorkflowNodeType.ACTIVITY;
+      }
+    };
+
+    WorkflowNode element = mock(WorkflowNode.class);
+    when(element.getId()).thenReturn("nodeId");
+    when(element.isConditional()).thenReturn(true);
+    when(element.getIfCondition("parentId")).thenReturn("${condition}");
+
+    ComplexGatewayBuilder builder = mock(ComplexGatewayBuilder.class);
+    when(builder.condition(Mockito.<String>any(), Mockito.<String>any())).thenReturn(builder);
+
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    when(context.isAlreadyBuilt("nodeId")).thenReturn(false);
+    when(context.hasEventSubProcess()).thenReturn(false);
+
+    // Act
+    underTest.connect(element, "parentId", builder, context);
+
+    // Assert
+    verify(builder).condition("if", "${condition}");
+  }
+
+  /**
+   * Test {@link AbstractNodeBpmnBuilder#connect(WorkflowNode, String, AbstractFlowNodeBuilder,
+   * BuildProcessContext)}.
+   *
+   * <ul>
+   *   <li>When already built, conditional, and builder is not a gateway; then exclusive gateway
+   *       is created for the conditional connection.
+   * </ul>
+   */
+  @Test
+  @DisplayName(
+      "Test connect(WorkflowNode, String, AbstractFlowNodeBuilder, BuildProcessContext); doConditionalConnection non-gateway path")
+  @SuppressWarnings("unchecked")
+  void testConnect_alreadyBuilt_conditional_nonGateway() {
+    // Arrange
+    WorkflowNode element = mock(WorkflowNode.class);
+    when(element.getId()).thenReturn("42");
+    when(element.isConditional()).thenReturn(true);
+    when(element.getIfCondition(Mockito.<String>any())).thenReturn("${cond}");
+
+    BoundaryEventBuilder builder = mock(BoundaryEventBuilder.class);
+    ExclusiveGatewayBuilder exclusiveGwBuilder = mock(ExclusiveGatewayBuilder.class);
+    AbstractFlowNodeBuilder<?, ?> connectToResult = mock(AbstractFlowNodeBuilder.class);
+    AbstractFlowNodeBuilder<?, ?> moveToResult = mock(AbstractFlowNodeBuilder.class);
+
+    when(builder.exclusiveGateway(anyString())).thenReturn(exclusiveGwBuilder);
+    when(exclusiveGwBuilder.condition(Mockito.<String>any(), Mockito.<String>any()))
+        .thenReturn(exclusiveGwBuilder);
+    when(exclusiveGwBuilder.connectTo(Mockito.<String>any())).thenReturn(connectToResult);
+    when(connectToResult.moveToNode(Mockito.<String>any())).thenReturn(moveToResult);
+
+    BuildProcessContext context2 = mock(BuildProcessContext.class);
+    when(context2.isAlreadyBuilt(Mockito.<String>any())).thenReturn(true);
+    when(context2.hasEventSubProcess()).thenReturn(false);
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        abstractNodeBpmnBuilder.connect(element, "parentId", builder, context2);
+
+    // Assert
+    verify(builder).exclusiveGateway("42" + EXCLUSIVE_GATEWAY_SUFFIX);
+    verify(exclusiveGwBuilder).condition("if", "${cond}");
+    verify(exclusiveGwBuilder).connectTo("42");
+    verify(connectToResult).moveToNode("42" + EXCLUSIVE_GATEWAY_SUFFIX);
+    verify(moveToResult).endEvent();
+    assertSame(builder, result);
+  }
+
+  /**
+   * Test {@link AbstractNodeBpmnBuilder#connect(WorkflowNode, String, AbstractFlowNodeBuilder,
+   * BuildProcessContext)}.
+   *
+   * <ul>
+   *   <li>When already built, conditional, non-gateway builder, and has event sub process; then
+   *       event sub process is ended before the conditional connection.
+   * </ul>
+   */
+  @Test
+  @DisplayName(
+      "Test connect(WorkflowNode, String, AbstractFlowNodeBuilder, BuildProcessContext); doConditionalConnection non-gateway with event sub process")
+  @SuppressWarnings("unchecked")
+  void testConnect_alreadyBuilt_conditional_nonGateway_withEventSubProcess() {
+    // Arrange
+    WorkflowNode element = mock(WorkflowNode.class);
+    when(element.getId()).thenReturn("42");
+    when(element.isConditional()).thenReturn(true);
+    when(element.getIfCondition(Mockito.<String>any())).thenReturn("${cond}");
+
+    BoundaryEventBuilder originalBuilder = mock(BoundaryEventBuilder.class);
+
+    EventSubProcessBuilder eventSubProcessBuilder = mock(EventSubProcessBuilder.class);
+    SubProcessBuilder subProcessDoneBuilder = mock(SubProcessBuilder.class);
+    ExclusiveGatewayBuilder exclusiveGwBuilder = mock(ExclusiveGatewayBuilder.class);
+    AbstractFlowNodeBuilder<?, ?> connectToResult = mock(AbstractFlowNodeBuilder.class);
+    AbstractFlowNodeBuilder<?, ?> moveToResult = mock(AbstractFlowNodeBuilder.class);
+
+    when(subProcessDoneBuilder.exclusiveGateway(anyString())).thenReturn(exclusiveGwBuilder);
+    when(exclusiveGwBuilder.condition(Mockito.<String>any(), Mockito.<String>any()))
+        .thenReturn(exclusiveGwBuilder);
+    when(exclusiveGwBuilder.connectTo(Mockito.<String>any())).thenReturn(connectToResult);
+    when(connectToResult.moveToNode(Mockito.<String>any())).thenReturn(moveToResult);
+
+    BuildProcessContext context2 = mock(BuildProcessContext.class);
+    when(context2.isAlreadyBuilt(Mockito.<String>any())).thenReturn(true);
+    when(context2.hasEventSubProcess()).thenReturn(true);
+    when(context2.removeLastEventSubProcessBuilder()).thenReturn(eventSubProcessBuilder);
+    when(eventSubProcessBuilder.subProcessDone()).thenReturn(subProcessDoneBuilder);
+
+    // Act
+    abstractNodeBpmnBuilder.connect(element, "parentId", originalBuilder, context2);
+
+    // Assert
+    verify(context2).removeLastEventSubProcessBuilder();
+    verify(context2).cacheSubProcessTimeoutToDone(subProcessDoneBuilder);
+    verify(subProcessDoneBuilder).exclusiveGateway("42" + EXCLUSIVE_GATEWAY_SUFFIX);
+    verify(exclusiveGwBuilder).condition("if", "${cond}");
   }
 }
--- workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/ActivityExpiredNodeBuilderDiffblueTest.java
+++ workflow-bot-app/src/test/java/com/symphony/bdk/workflow/engine/camunda/bpmn/builder/ActivityExpiredNodeBuilderDiffblueTest.java
@@ -1,16 +1,38 @@
 package com.symphony.bdk.workflow.engine.camunda.bpmn.builder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
+import com.symphony.bdk.workflow.engine.WorkflowNode;
 import com.symphony.bdk.workflow.engine.WorkflowNodeType;
+import com.symphony.bdk.workflow.engine.camunda.bpmn.BuildProcessContext;
+import com.symphony.bdk.workflow.swadl.v1.EventWithTimeout;
+import com.symphony.bdk.workflow.swadl.v1.activity.ExecuteScript;
+
+import org.camunda.bpm.model.bpmn.builder.AbstractCatchEventBuilder;
+import org.camunda.bpm.model.bpmn.builder.AbstractFlowNodeBuilder;
+import org.camunda.bpm.model.bpmn.builder.AbstractGatewayBuilder;
+import org.camunda.bpm.model.bpmn.builder.BoundaryEventBuilder;
+import org.camunda.bpm.model.bpmn.builder.EventSubProcessBuilder;
+import org.camunda.bpm.model.bpmn.builder.IntermediateCatchEventBuilder;
+import org.camunda.bpm.model.bpmn.builder.ScriptTaskBuilder;
+import org.camunda.bpm.model.bpmn.builder.SubProcessBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
 
 @ContextConfiguration(classes = {ActivityExpiredNodeBuilder.class})
 @ExtendWith(SpringExtension.class)
@@ -30,5 +52,210 @@ class ActivityExpiredNodeBuilderDiffblueTest {
   void testType() {
     // Arrange, Act and Assert
     assertEquals(WorkflowNodeType.ACTIVITY_EXPIRED_EVENT, activityExpiredNodeBuilder.type());
+  }
+
+  /**
+   * Test {@link ActivityExpiredNodeBuilder#build} when there is a non-exclusive form reply parent
+   * and a timeout subprocess is present.
+   *
+   * <p>Method under test: {@link ActivityExpiredNodeBuilder#build}
+   */
+  @Test
+  @DisplayName("Test build() - non-exclusive form reply parent, has timeout subprocess")
+  @SuppressWarnings("unchecked")
+  void testBuild_hasNoExclusiveFormReplyParent_hasTimeoutSubProcess() {
+    // Arrange
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    WorkflowNode element = mock(WorkflowNode.class);
+    WorkflowNode parentNode = mock(WorkflowNode.class);
+    AbstractFlowNodeBuilder<?, ?> inputBuilder = mock(AbstractFlowNodeBuilder.class);
+
+    when(element.getId()).thenReturn("nodeId");
+    when(context.getParents("nodeId")).thenReturn(Collections.singletonList("parentId"));
+    when(context.readWorkflowNode("parentId")).thenReturn(parentNode);
+    when(parentNode.isNotExclusiveFormReply()).thenReturn(true);
+    when(context.hasTimeoutSubProcess()).thenReturn(true);
+
+    AbstractFlowNodeBuilder<?, ?> timeoutBuilder = mock(AbstractFlowNodeBuilder.class);
+    doReturn(timeoutBuilder).when(context).removeLastSubProcessTimeoutBuilder();
+
+    ScriptTaskBuilder stb6 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb5 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb4 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb3 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb2 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stbFinal = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb7 = mock(ScriptTaskBuilder.class);
+
+    when(timeoutBuilder.scriptTask()).thenReturn(stb7);
+    when(stb7.id(any())).thenReturn(stb6);
+    when(stb6.name(any())).thenReturn(stb5);
+    when(stb5.camundaAsyncAfter()).thenReturn(stb4);
+    when(stb4.scriptText(any())).thenReturn(stb3);
+    when(stb3.scriptFormat(any())).thenReturn(stb2);
+    when(stb2.camundaExecutionListenerClass(any(), Mockito.<Class<Object>>any()))
+        .thenReturn(stbFinal);
+
+    when(element.getActivity()).thenReturn(new ExecuteScript());
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        activityExpiredNodeBuilder.build(element, "parentId", inputBuilder, context);
+
+    // Assert
+    assertSame(stbFinal, result);
+  }
+
+  /**
+   * Test {@link ActivityExpiredNodeBuilder#build} when there is a non-exclusive form reply parent
+   * and no timeout subprocess is present.
+   *
+   * <p>Method under test: {@link ActivityExpiredNodeBuilder#build}
+   */
+  @Test
+  @DisplayName("Test build() - non-exclusive form reply parent, no timeout subprocess")
+  @SuppressWarnings("unchecked")
+  void testBuild_hasNoExclusiveFormReplyParent_noTimeoutSubProcess() {
+    // Arrange
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    WorkflowNode element = mock(WorkflowNode.class);
+    WorkflowNode parentNode = mock(WorkflowNode.class);
+    AbstractFlowNodeBuilder<?, ?> inputBuilder = mock(AbstractFlowNodeBuilder.class);
+
+    when(element.getId()).thenReturn("nodeId");
+    when(context.getParents("nodeId")).thenReturn(Collections.singletonList("parentId"));
+    when(context.readWorkflowNode("parentId")).thenReturn(parentNode);
+    when(parentNode.isNotExclusiveFormReply()).thenReturn(true);
+    when(context.hasTimeoutSubProcess()).thenReturn(false);
+
+    EventSubProcessBuilder eventSubProcessBuilder = mock(EventSubProcessBuilder.class);
+    SubProcessBuilder subProcessBuilder = mock(SubProcessBuilder.class);
+    BoundaryEventBuilder boundaryEventBuilder = mock(BoundaryEventBuilder.class);
+
+    when(context.removeLastEventSubProcessBuilder()).thenReturn(eventSubProcessBuilder);
+    when(eventSubProcessBuilder.subProcessDone()).thenReturn(subProcessBuilder);
+    when(subProcessBuilder.boundaryEvent()).thenReturn(boundaryEventBuilder);
+    when(boundaryEventBuilder.error(any())).thenReturn(boundaryEventBuilder);
+
+    ScriptTaskBuilder stb6 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb5 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb4 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb3 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb2 = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stbFinal = mock(ScriptTaskBuilder.class);
+    ScriptTaskBuilder stb7 = mock(ScriptTaskBuilder.class);
+
+    when(boundaryEventBuilder.scriptTask()).thenReturn(stb7);
+    when(stb7.id(any())).thenReturn(stb6);
+    when(stb6.name(any())).thenReturn(stb5);
+    when(stb5.camundaAsyncAfter()).thenReturn(stb4);
+    when(stb4.scriptText(any())).thenReturn(stb3);
+    when(stb3.scriptFormat(any())).thenReturn(stb2);
+    when(stb2.camundaExecutionListenerClass(any(), Mockito.<Class<Object>>any()))
+        .thenReturn(stbFinal);
+
+    when(element.getActivity()).thenReturn(new ExecuteScript());
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        activityExpiredNodeBuilder.build(element, "parentId", inputBuilder, context);
+
+    // Assert
+    assertSame(stbFinal, result);
+  }
+
+  /**
+   * Test {@link ActivityExpiredNodeBuilder#build} when builder is an AbstractCatchEventBuilder.
+   *
+   * <p>Method under test: {@link ActivityExpiredNodeBuilder#build}
+   */
+  @Test
+  @DisplayName("Test build() - no exclusive form reply parent, catch event builder")
+  @SuppressWarnings("unchecked")
+  void testBuild_noExclusiveFormReplyParent_catchEventBuilder() {
+    // Arrange
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    WorkflowNode element = mock(WorkflowNode.class);
+    AbstractCatchEventBuilder<?, ?> catchEventBuilder = mock(AbstractCatchEventBuilder.class);
+    AbstractCatchEventBuilder<?, ?> timerBuilder = mock(AbstractCatchEventBuilder.class);
+
+    when(element.getId()).thenReturn("nodeId");
+    when(context.getParents("nodeId")).thenReturn(Collections.emptyList());
+
+    EventWithTimeout event = new EventWithTimeout();
+    event.setTimeout("PT1H");
+    when(element.getEvent()).thenReturn(event);
+    when(catchEventBuilder.timerWithDuration("PT1H")).thenReturn((AbstractCatchEventBuilder) timerBuilder);
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        activityExpiredNodeBuilder.build(element, "parentId", catchEventBuilder, context);
+
+    // Assert
+    assertSame(timerBuilder, result);
+  }
+
+  /**
+   * Test {@link ActivityExpiredNodeBuilder#build} when builder is an AbstractGatewayBuilder.
+   *
+   * <p>Method under test: {@link ActivityExpiredNodeBuilder#build}
+   */
+  @Test
+  @DisplayName("Test build() - no exclusive form reply parent, gateway builder")
+  @SuppressWarnings("unchecked")
+  void testBuild_noExclusiveFormReplyParent_gatewayBuilder() {
+    // Arrange
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    WorkflowNode element = mock(WorkflowNode.class);
+    AbstractGatewayBuilder<?, ?> gatewayBuilder = mock(AbstractGatewayBuilder.class);
+    IntermediateCatchEventBuilder catchEventBuilder = mock(IntermediateCatchEventBuilder.class);
+    IntermediateCatchEventBuilder namedBuilder = mock(IntermediateCatchEventBuilder.class);
+    IntermediateCatchEventBuilder timerBuilder = mock(IntermediateCatchEventBuilder.class);
+
+    when(element.getId()).thenReturn("nodeId");
+    when(context.getParents("nodeId")).thenReturn(Collections.emptyList());
+
+    EventWithTimeout event = new EventWithTimeout();
+    event.setTimeout("PT1H");
+    when(element.getEvent()).thenReturn(event);
+    when(gatewayBuilder.intermediateCatchEvent()).thenReturn(catchEventBuilder);
+    when(catchEventBuilder.name("nodeId")).thenReturn(namedBuilder);
+    when(namedBuilder.timerWithDuration("PT1H")).thenReturn(timerBuilder);
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        activityExpiredNodeBuilder.build(element, "parentId", gatewayBuilder, context);
+
+    // Assert
+    assertSame(timerBuilder, result);
+  }
+
+  /**
+   * Test {@link ActivityExpiredNodeBuilder#build} when builder is a generic AbstractFlowNodeBuilder
+   * (neither CatchEvent nor Gateway).
+   *
+   * <p>Method under test: {@link ActivityExpiredNodeBuilder#build}
+   */
+  @Test
+  @DisplayName("Test build() - no exclusive form reply parent, generic builder")
+  void testBuild_noExclusiveFormReplyParent_genericBuilder() {
+    // Arrange
+    BuildProcessContext context = mock(BuildProcessContext.class);
+    WorkflowNode element = mock(WorkflowNode.class);
+    AbstractFlowNodeBuilder<?, ?> builder = mock(AbstractFlowNodeBuilder.class);
+
+    when(element.getId()).thenReturn("nodeId");
+    when(context.getParents("nodeId")).thenReturn(Collections.emptyList());
+
+    EventWithTimeout event = new EventWithTimeout();
+    event.setTimeout("PT1H");
+    when(element.getEvent()).thenReturn(event);
+
+    // Act
+    AbstractFlowNodeBuilder<?, ?> result =
+        activityExpiredNodeBuilder.build(element, "parentId", builder, context);
+
+    // Assert
+    assertSame(builder, result);
   }
 }
